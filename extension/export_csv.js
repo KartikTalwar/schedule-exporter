88e7faeb32b3c5815af630c989227fadfbabe7fe
Kartik <talwar.kartik@gmail.com>
2011-12-10 19:18:32 -0500

    Encoding URL Update

--- extension/export_csv.js
+++ extension/export_csv.js
@@ -1,10 +1,3 @@
-/**
-*
-*  Base64 encode / decode
-*  http://www.webtoolkit.info/
-*
-**/
- 
 var Base64 = {
  
 	// private property
@@ -44,42 +37,6 @@ var Base64 = {
 		return output;
 	},
  
-	// public method for decoding
-	decode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
- 
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
- 
-		while (i < input.length) {
- 
-			enc1 = this._keyStr.indexOf(input.charAt(i++));
-			enc2 = this._keyStr.indexOf(input.charAt(i++));
-			enc3 = this._keyStr.indexOf(input.charAt(i++));
-			enc4 = this._keyStr.indexOf(input.charAt(i++));
- 
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
- 
-			output = output + String.fromCharCode(chr1);
- 
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
- 
-		}
- 
-		output = Base64._utf8_decode(output);
- 
-		return output;
- 
-	},
  
 	// private method for UTF-8 encoding
 	_utf8_encode : function (string) {
@@ -107,38 +64,7 @@ var Base64 = {
  
 		return utftext;
 	},
- 
-	// private method for UTF-8 decoding
-	_utf8_decode : function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
- 
-		while ( i < utftext.length ) {
- 
-			c = utftext.charCodeAt(i);
- 
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
- 
-		}
- 
-		return string;
-	}
- 
+  
 }
 
 
@@ -321,9 +247,9 @@ if(document.getElementsByClassName('PSPAGECONTAINER').length > 0){
 		
 	jQuery.ajax({
 	  type: 'GET',
-	  url: 'http://opendata.uwaterloo.ca/youwaterloo/api/tools/ScheduleExporter.php',
+	  url: 'http://opendata.uwaterloo.ca/youwaterloo/tools/ScheduleExporter/ScheduleExporter.php',
 	  data: {'data': Base64.encode(result) }
-	 	});
+	 	    });
 			
 //	var newdiv = document.createElement('div');
 //	text = result.join("<br/>");
